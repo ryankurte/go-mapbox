cac1a8a7cc709e6175969702c73652887d1a3562
Ryan Kurte <ryankurte@gmail.com>
2017-03-16 16:34:51 +1300

    Added geocode module to main

--- lib/mapbox.go
+++ lib/mapbox.go
@@ -2,16 +2,24 @@ package mapbox
 
 import (
 	"github.com/ryankurte/go-mapbox/lib/base"
+	"github.com/ryankurte/go-mapbox/lib/geocode"
 )
 
+// Mapbox API Wrapper structure
 type Mapbox struct {
-	base *base.Base
+	base    *base.Base
+	Geocode *geocode.Geocode
 }
 
+// NewMapbox Create a new mapbox API instance
 func NewMapbox(token string) *Mapbox {
 	m := &Mapbox{}
 
+	// Create base instance
 	m.base = base.NewBase(token)
+
+	// Bind modules
+	m.Geocode = geocode.NewGeocode(m.base)
 
 	return m
 }
