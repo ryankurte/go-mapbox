1711034eae8efc1ede695cdf5542e1a91a751bf9
Ryan Kurte <ryankurte@gmail.com>
2017-03-18 15:42:15 +1300

    Added vector file type, fetch not yet supported

--- lib/maps/types.go
+++ lib/maps/types.go
@@ -23,13 +23,14 @@ const (
 type MapFormat string
 
 const (
-	MapFormatPng    MapFormat = "png"    // true color PNG
-	MapFormatPng32  MapFormat = "png32"  // 32 color indexed PNG
-	MapFormatPng64  MapFormat = "png64"  // 64 color indexed PNG
-	MapFormatPng128 MapFormat = "png128" // 128 color indexed PNG
-	MapFormatPng256 MapFormat = "png256" // 256 color indexed PNG
-	MapFormatPngRaw MapFormat = "pngraw" // Raw PNG (only for MapIDTerrainRGB)
-	MapFormatJpg70  MapFormat = "jpg70"  // 70% quality JPG
-	MapFormatJpg80  MapFormat = "jpg80"  // 80% quality JPG
-	MapFormatJpg90  MapFormat = "jpg90"  // 90% quality JPG
+	MapFormatPng        MapFormat = "png"    // true color PNG
+	MapFormatPng32      MapFormat = "png32"  // 32 color indexed PNG
+	MapFormatPng64      MapFormat = "png64"  // 64 color indexed PNG
+	MapFormatPng128     MapFormat = "png128" // 128 color indexed PNG
+	MapFormatPng256     MapFormat = "png256" // 256 color indexed PNG
+	MapFormatPngRaw     MapFormat = "pngraw" // Raw PNG (only for MapIDTerrainRGB)
+	MapFormatJpg70      MapFormat = "jpg70"  // 70% quality JPG
+	MapFormatJpg80      MapFormat = "jpg80"  // 80% quality JPG
+	MapFormatJpg90      MapFormat = "jpg90"  // 90% quality JPG
+	MapFormatVectorTile MapFormat = "mvt"    // Vector Tile
 )
