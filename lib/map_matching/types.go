489dd07363b805f859937333351249f793679b18
Ryan Kurte <ryankurte@gmail.com>
2017-05-11 11:36:04 +1200

    Approach to solve issue #12 using interface{} and casting. Would be neat to know if there is a better way.

--- lib/map_matching/types.go
+++ lib/map_matching/types.go
@@ -9,6 +9,10 @@
 
 package mapmatching
 
+import (
+	"fmt"
+)
+
 // MatchingResponse is the response from GetMatching
 // https://www.mapbox.com/api-documentation/#match-response-object
 type MatchingResponse struct {
@@ -17,28 +21,89 @@ type MatchingResponse struct {
 	Tracepoint []TracePoint
 }
 
+type Coordinate []float64
+
+type GeojsonGeometry struct {
+	Coordinates []Coordinate
+}
+
+type PolylineGeometry string
+
 // Matchings it a route object with additional confidence field
 // https://www.mapbox.com/api-documentation/#match-object
 type Matchings struct {
 	Confidence float64
-	Distance   float32
-	Duration   float32
-	Geometry   string
+	Distance   float64
+	Duration   float64
+	Geometry   interface{} // Issue: must support polyline (string) or geojson (object)
 	Legs       []MatchingLeg
+}
+
+func (m *Matchings) GetGeometryGeojson() (*GeojsonGeometry, error) {
+	geojson, ok := m.Geometry.(map[string]interface{})
+	if !ok {
+		return nil, fmt.Errorf("Malformed geojson geometry (expected map[string]interface, received %t)", m.Geometry)
+	}
+
+	t, ok := geojson["type"]
+	if !ok {
+		return nil, fmt.Errorf("Malformed geojson geometry (no type defined)")
+	}
+	if t != "LineString" {
+		return nil, fmt.Errorf("Malformed geojson geometry (incorrect type name: %s)", t)
+	}
+
+	v, ok := geojson["coordinates"]
+	if !ok {
+		return nil, fmt.Errorf("Malformed geojson geometry (no coordinates defined)")
+	}
+	values, ok := v.([]interface{})
+	//values, ok := v.([][]float64)
+	if !ok {
+		return nil, fmt.Errorf("Malformed geojson geometry (coordinates are not an array of float pairs)")
+	}
+
+	geometry := GeojsonGeometry{}
+	for _, v := range values {
+		value, ok := v.([]interface{})
+		if !ok {
+			return nil, fmt.Errorf("Could not cast value to coordinate slice (type: %t)", v)
+		}
+		lat, ok := value[0].(float64)
+		if !ok {
+			return nil, fmt.Errorf("Error casting lat (type: %t)", value[0])
+		}
+		lng, ok := value[1].(float64)
+		if !ok {
+			return nil, fmt.Errorf("Error casting lng (type: %t)", value[1])
+		}
+
+		geometry.Coordinates = append(geometry.Coordinates, []float64{lat, lng})
+	}
+
+	return &geometry, nil
+}
+
+func (m *Matchings) GetGeometryPolyline() (string, error) {
+	g, ok := m.Geometry.(string)
+	if !ok {
+		return "", fmt.Errorf("Non polyline geometry (type: %t)", m.Geometry)
+	}
+	return g, nil
 }
 
 //MatchingLeg legs inside the matching object
 type MatchingLeg struct {
-	Step     []float32
+	Step     []float64
 	Summary  string
-	Duration float32
-	Distance float32
+	Duration float64
+	Distance float64
 }
 
 // TracePoint represents the location an input point was matched with
 type TracePoint struct {
 	WaypointIndex  int16
-	Location       []float32
+	Location       []float64
 	Name           string
 	MatchingsIndex int16
 }
